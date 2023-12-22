5be7b8d45450cb49fb9a53708c447f44274337a2
Pamir Perdeci <pamirperdeci@gmail.com>
2023-12-22 16:49:03 -0500

    Authorization works

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,24 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  
   // Retrieve the token from the Authorization header
   const token = req.headers.authorization;
-
   // Check if the token is missing
   if (!token) {
-    return res.status(401).json({ message: "token required" });
+    return res.status(401).json({ message: "what? no token" });
+  }else{
+    jwt.verify(token,process.env.JWT_SECRET,(error,decoded)=>{
+      if(error){
+        res.status(401).json({message: "your token is invalid or expired"})
+      }else {
+        req.decodedjwt = decoded;
+        next();
+      }
+    } )
   }
+  //CHECK IF TOKEN IS VALID
+  console.log(token)
 
-  // Verify the token
-  try {
-    // Replace 'process.env.JWT_SECRET' with your secret key
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded)
-    req.user = decoded; // Optional: Add the decoded token to the request if you want to use it later
 
-    next(); // Token is valid, proceed to the next middleware
-  } catch (err) {
-    // Token is invalid or expired
-    return res.status(401).json({ message: "token invalid" });
-  }
 };
